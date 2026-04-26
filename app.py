3630ebe4de705b932b445f3ff614c11ea0060e4e
Amogh Patel <amoghpatel@Surajs-MacBook-Pro.local>
2026-04-26 16:28:29 -0700

    adding bug

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ def multiplication(x, y):
     return x * y
 
 if __name__ == "__main__":
-    print("Multiplication Result: ", multiplication(3, 4))
+    print("Multiplication Result: "+ multiplication(3, 4))
